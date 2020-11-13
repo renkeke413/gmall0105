ede148a09d301be27a93623ec4e7e16275ad5846
11519 <1151915528@qq.com>
2020-11-13 15:56:00 +0800

    user模块第一次提交

---
+++ gmall-user/src/main/java/com/huida/gmall/user/GmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.huida.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserApplication.class, args);
+    }
+
+}
---
+++ gmall-user/src/test/java/com/huida/gmall/user/GmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huida.gmall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
